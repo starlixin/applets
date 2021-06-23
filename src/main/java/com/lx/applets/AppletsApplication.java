e1573a2330b5ca6e10c46cb9393daa64fe2478e5
Xin.Li <xin.li@androidmov.com>
2021-06-23 15:24:44 +0800

    init

---
+++ src/main/java/com/lx/applets/AppletsApplication.java
@@ -0,0 +1,13 @@
+package com.lx.applets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppletsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppletsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lx/applets/AppletsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lx.applets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppletsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
